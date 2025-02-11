c35571b12758188101c855e30ecb226170b7cd3c
Catfish1210 <chris.laks@gmail.com>
2025-02-11 19:10:50 +0200

    Update app's html/css, create basic component structure

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,47 +1,70 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+    import Templates from './lib/Templates.svelte';
 </script>
 
 <main>
-  <div>
-    <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+  <h1 class="glow title">Certificate Generator</h1>
+
+  <div class="section-container">
+    <div id="template-section" class="section">
+      <Templates />
+    </div>
+    <div class="section">
+      
+    </div>
+    <div class="section">
+      
+    </div>
   </div>
-  <h1>Vite + Svelte</h1>
 
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
+  #template-section {
+    display: flex;
+    justify-content: center;
+    width: 50vh;
+  }
+
+  .section {
+    height: 75vh;
+    width: 90%;
+    background-color: rgba(185, 185, 185, 0.163);
+    border: 2px solid whitesmoke;
+    border-radius: 9px;
+  }
+
+  .section-container {
+    display: flex;
+    gap: 2vh;
+    justify-content: start;
+    padding-left: 1.5vh;
+    padding-right: 1.5vh;
+
+    align-items: center;
+    overflow: hidden;
+    height: 80vh;
+    min-width: 130vh;
+    max-width: 90%;
+    background-color: rgba(185, 185, 185, 0.163);
+    margin: 2vh;
+    margin-top: 0;
+    border: 3px solid whitesmoke;
+    border-radius: 12px;
+  }
+
+  .title {
+    text-wrap-mode: nowrap;
+
+    margin: 0;
+    margin-top: 2vh;
+  }
+  .glow {
+    user-select: none;
     will-change: filter;
     transition: filter 300ms;
   }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
+  .glow:hover {
+    filter: drop-shadow(0 0 0.8em #212bede1);
   }
 </style>

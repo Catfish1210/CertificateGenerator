5ba0768125e3d78266411a96c10db46ef52086f1
Catfish1210 <chris.laks@gmail.com>
2025-02-12 15:23:34 +0200

    Include ActiveForm component & adjust css

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,70 +1,80 @@
 <script>
-    import Templates from './lib/Templates.svelte';
+	import ActiveForm from "./lib/ActiveForm.svelte";
+	import Templates from "./lib/Templates.svelte";
+	import { selectedTemplateId } from "./store";
 </script>
 
 <main>
-  <h1 class="glow title">Certificate Generator</h1>
+	<h1 class="glow title">Certificate Generator</h1>
 
-  <div class="section-container">
-    <div id="template-section" class="section">
-      <Templates />
-    </div>
-    <div class="section">
-      
-    </div>
-    <div class="section">
-      
-    </div>
-  </div>
+	<div class="section-container">
+		<div id="template-section" class="section">
+			<div>
+				<h3 style="margin-top:0.5rem; margin-left: 0.5rem; user-select: none;"> Templates </h3>
+				<Templates />
+			</div>
+		</div>
+		{#if $selectedTemplateId !== null}
+			<div id="form-section" class="section">
+				<div>
+					<h3 style="margin: 0; margin-top:0.5rem; margin-left: 0.5rem; user-select: none;">Form</h3>
+					<ActiveForm />
+				</div>
+			</div>
+		{/if}
+		<div class="section">
 
+		</div>
+	</div>
 </main>
 
 <style>
-  #template-section {
-    display: flex;
-    justify-content: center;
-    width: 50vh;
-  }
+	#template-section {
+		display: flex;
+		justify-content: center;
+		width: 50vh;
+	}
 
-  .section {
-    height: 75vh;
-    width: 90%;
-    background-color: rgba(185, 185, 185, 0.163);
-    border: 2px solid whitesmoke;
-    border-radius: 9px;
-  }
+	.section {
+		height: 75vh;
+		width: 90%;
+		background-color: rgba(185, 185, 185, 0.163);
+		border: 2px solid whitesmoke;
+		border-radius: 9px;
+		overflow: scroll;
+	}
 
-  .section-container {
-    display: flex;
-    gap: 2vh;
-    justify-content: start;
-    padding-left: 1.5vh;
-    padding-right: 1.5vh;
+	.section-container {
+		display: flex;
+		gap: 2vh;
+		justify-content: start;
+		padding-left: 1.5vh;
+		padding-right: 1.5vh;
+		align-items: center;
+		overflow: hidden;
+		height: 80vh;
+		min-width: 130vh;
+		max-width: 90%;
+		background-color: rgba(185, 185, 185, 0.163);
+		margin: 2vh;
+		margin-top: 0;
+		border: 3px solid whitesmoke;
+		border-radius: 12px;
+	}
 
-    align-items: center;
-    overflow: hidden;
-    height: 80vh;
-    min-width: 130vh;
-    max-width: 90%;
-    background-color: rgba(185, 185, 185, 0.163);
-    margin: 2vh;
-    margin-top: 0;
-    border: 3px solid whitesmoke;
-    border-radius: 12px;
-  }
+	.title {
+		text-wrap-mode: nowrap;
+		margin: 0;
+		margin-top: 2vh;
+	}
+	
+	.glow {
+		user-select: none;
+		will-change: filter;
+		transition: filter 300ms;
+	}
 
-  .title {
-    text-wrap-mode: nowrap;
-
-    margin: 0;
-    margin-top: 2vh;
-  }
-  .glow {
-    user-select: none;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .glow:hover {
-    filter: drop-shadow(0 0 0.8em #212bede1);
-  }
+	.glow:hover {
+		filter: drop-shadow(0 0 0.8em #212bede1);
+	}
 </style>

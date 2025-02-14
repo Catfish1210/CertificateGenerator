a337c39fea05598cbe5a3202c59ba22a1a054449
Catfish1210 <chris.laks@gmail.com>
2025-02-14 02:55:33 +0200

    Adjust format

--- backend/errorHandler.js
+++ backend/errorHandler.js
@@ -1,27 +1,27 @@
 // https://docs.pdfgeneratorapi.com/v4/#section/Error-codes
 const handleApiError = (statusCode, message, res) => {
-    let error = message || "Unknown error has occured.";
+    let error;
     switch (statusCode) {
         case 401:
-            error = message || "Unauthorized: Authentication failed.";
+            error = message || 'Unauthorized: Authentication failed.';
             break;
         case 402:
-            error = message || "Payment Required: Your account is suspended, please upgrade your account.";
+            error = message || 'Payment Required: Your account is suspended, please upgrade your account.';
             break;
         case 403:
-            error = message || "Forbidden";
+            error = message || 'Forbidden';
             break;
         case 404:
-            error = message || "Not Found";
+            error = message || 'Not Found';
             break;
         case 422:
-            error = message || "Unprocessable Entity";
+            error = message || 'Unprocessable Entity';
             break;
         case 429:
-            error = message || "Too Many Requests: You can make up to 2 requests per second and 60 requests per minute.";
+            error = message || 'Too Many Requests: You can make up to 2 requests per second and 60 requests per minute.';
             break;
         default:
-            error = message || "An unknown error occurred.";
+            error = message || 'An unknown error has occurred.';
             break;
     }
 
@@ -29,7 +29,7 @@ const handleApiError = (statusCode, message, res) => {
 };
 
 const handleCatchError = (err, res) => {
-    console.error("Error:", err);
+    console.error('Error:', err);
     res.status(500).json({ error: `Request Failed: ${err}` });
 };
 

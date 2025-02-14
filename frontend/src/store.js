c01d2ce53ccb336d41497a28d0484f7c37d93686
Catfish1210 <chris.laks@gmail.com>
2025-02-14 09:34:38 +0200

    Update store with new component variables for docHistory feature

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,8 +1,11 @@
-import { writable } from "svelte/store";
+import { writable, get } from "svelte/store";
 
 export const generatedPdf = writable(null);
 export const CertificateTemplateId = writable(null);
 export const selectedTemplateId = writable(null);
+export const showDocumentHistory = writable(false);
+export const selectedDocumentId = writable(null);
+export const documentHistoryArr = writable([]);
 export const formData = writable({
     date: null,
     image: null,
@@ -10,3 +13,15 @@ export const formData = writable({
     student_name: null,
     subject: null,
 });
+
+export const updateDocumentHistory = async () => {
+    const templateId = get(CertificateTemplateId);
+    try {
+        const response = await fetch(`/api/document-history?templateId=${templateId}`);
+        if (!response.ok) throw new Error("Failed to fetch document history");
+        const data = await response.json();
+        documentHistoryArr.set(data.documentHistory);
+    } catch (err) {
+        console.error(err);
+    }
+}
